34672f8f4fe5a9aee7329de0260b35dee8e56603
Ikyrillos <kyrillosmaherfekri@gmail.com>
2024-04-09 23:05:00 +0200

    catch uncaught exceptions + unhandledRejections

--- server.js
+++ server.js
@@ -2,6 +2,12 @@ const dotenv = require('dotenv');
 
 const mongoose = require('mongoose');
 
+process.on('uncaughtException', (err) => {
+    console.log('UNCAUGHT EXCEPTION! 💥 Shutting down...');
+    console.log(err.name, err.message);
+    process.exit(1);
+});
+
 dotenv.config({ path: './config.env' });
 const app = require('./app');
 
@@ -10,10 +16,20 @@ const DB = process.env.DATABASE.replace(
     process.env.DATABASE_PASSWORD,
 );
 
-mongoose.connect(DB).then(() => console.log('connection to DB is successful'));
+mongoose.connect(DB).then(() => {
+    console.log('connection to DB is successful');
+});
 
 const port = 3000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
     // eslint-disable-next-line no-console
     console.log(`Server started on http://localhost:${port}`);
+});
+
+process.on('unhandledRejection', (err) => {
+    console.log('UNHANDLED REJECTION! Shutting down...');
+    console.log(err.name, err.message);
+    server.close(() => {
+        process.exit(1);
+    });
 });
