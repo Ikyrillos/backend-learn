ebd54ab3325cd0dceb67567464200f6cb1e9bdc3
Ikyrillos <kyrillosmaherfekri@gmail.com>
2024-04-13 01:58:20 +0200

    add : send email for forget password

--- models/userModel.js
+++ models/userModel.js
@@ -1,6 +1,7 @@
 const bcrypt = require('bcryptjs/dist/bcrypt');
 const { default: mongoose } = require('mongoose');
 const validator = require('validator');
+const crypto = require('crypto');
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -39,6 +40,8 @@ const userSchema = new mongoose.Schema({
         lowercase: true,
         validate: [validator.isEmail, 'email is not valid'],
     },
+    passwordResetToken: String,
+    passwordResetExpires: Date,
     photo: String,
 });
 userSchema.pre('save', async function (next) {
@@ -68,6 +71,21 @@ userSchema.methods.changedPasswordAfter = function (JWTTimestamp) {
     // False means NOT changed
     return false;
 };
+userSchema.methods.createPasswordResetToken = function () {
+    const resetToken = crypto.randomBytes(32).toString('hex');
+
+    this.passwordResetToken = crypto
+        .createHash('sha256')
+        .update(resetToken)
+        .digest('hex');
+
+    console.log({ resetToken }, this.passwordResetToken);
+
+    this.passwordResetExpires = Date.now() + 10 * 60 * 1000;
+    console.log(this.passwordResetToken, this.resetToken);
+    return resetToken;
+};
+
 const User = mongoose.model('User', userSchema);
 
 module.exports = User;
