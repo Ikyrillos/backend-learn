908a00dbe8f383c66cee84ed1cd948d90652184b
Ikyrillos <kyrillosmaherfekri@gmail.com>
2024-04-13 06:11:12 +0200

    added:   rateLimit, helmet and json size limit Changes to be committed: 	modified:   app.js 	modified:   node_modules/.package-lock.json 	modified:   package-lock.json 	modified:   package.json

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
 const tourRoutes = require('./routes/tourRoutes');
 const userRoutes = require('./routes/userRoutes');
 const globalErrorHandler = require('./controllers/errorController');
@@ -7,6 +9,11 @@ const AppError = require('./utils/appError');
 
 const app = express();
 
+// 1) GLOBAL MIDDLEWARES
+// Set security HTTP headers
+app.use(helmet());
+
+// Development logging
 if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'));
     app.use((req, res, next) => {
@@ -15,15 +22,23 @@ if (process.env.NODE_ENV === 'development') {
         next();
     });
 }
+// Limit requests from same API
+const limiter = rateLimit({
+    max: 100,
+    windowMs: 60 * 60 * 1000,
+    message: 'Too many requests from this IP, please try again in an hour!',
+});
+app.use('/api', limiter);
 
 // middleWares
-app.use(express.json());
+app.use(express.json({ limit: '10kb' }));
 app.use(express.urlencoded({ extended: true }));
 
 //Routes
 app.use('/api/v1/tours', tourRoutes);
 app.use('/api/v1/users', userRoutes);
 
+// handle unhandled routes
 app.use('*', (req, res, next) => {
     console.log(res);
     next(
@@ -31,6 +46,7 @@ app.use('*', (req, res, next) => {
     );
 });
 
+// global error handler
 app.use(globalErrorHandler);
 
 module.exports = app;
